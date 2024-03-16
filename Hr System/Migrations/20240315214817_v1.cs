32c27a9fe138cb064a19c552e07c2854eb1a178c
Mohamed Elsayed <77064276+MohamedElsayed99@users.noreply.github.com>
2024-03-16 04:02:37 +0200

    last update

--- Hr System/Migrations/20240315214817_v1.cs
+++ Hr System/Migrations/20240315214817_v1.cs
@@ -10,20 +10,13 @@ namespace Hr_System.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "IsDeleted",
-                table: "Departments");
+
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<bool>(
-                name: "IsDeleted",
-                table: "Departments",
-                type: "bit",
-                nullable: false,
-                defaultValue: false);
+
         }
     }
 }
